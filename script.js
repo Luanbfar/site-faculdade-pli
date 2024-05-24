77b15f64420f066cbe595c916734b736cdba7f5d
Luan Farias <luanbfarias@outlook.com>
2024-05-24 10:48:15 -0300

    Added animations and changed navbar, footer, cards style

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll(".card");
+
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  const handleScroll = () => {
+    cards.forEach((card) => {
+      if (isElementInViewport(card)) {
+        card.classList.add("slide-in-left");
+      }
+    });
+  };
+
+  handleScroll();
+
+  window.addEventListener("scroll", handleScroll);
+  window.addEventListener("resize", handleScroll);
+});
