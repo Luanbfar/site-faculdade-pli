e63c4b7e09bae87dad80d32f883600d7c08a005b
Luan Farias <luanbfarias@outlook.com>
2024-06-04 05:24:10 -0300

    Fixed data validation

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ async function fetchCars() {
   const cars = await response.json();
   const carsDiv = document.getElementById("cars");
   carsDiv.innerHTML = "";
+  if (cars.length == 0) {
+    const noCars = document.createElement("div");
+    noCars.className =
+      "container mt-5 d-flex justify-content-center align-items-center text-light";
+    noCars.innerHTML = "Sem carros";
+    carsDiv.appendChild(noCars);
+  }
   cars.forEach((car) => {
     const carElement = document.createElement("div");
     carElement.className =
@@ -29,27 +36,25 @@ async function fetchCars() {
 async function addCar(event) {
   event.preventDefault();
   const name = document.getElementById("name").value;
-  const price = document.getElementById("price").value;
-  const quantity = document.getElementById("quantity").value;
-  await fetch("http://localhost:3000/api/cars", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name, price, quantity }),
-  });
+  const price = Number(document.getElementById("price").value);
+  const quantity = Number(document.getElementById("quantity").value);
+  if (!name || !price || !quantity) {
+    alert("Insira valores válidos");
+  } else {
+    await fetch("http://localhost:3000/api/cars", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, price, quantity }),
+    });
+  }
   fetchCars();
 }
 
 async function editCar(id, name, price, quantity) {
-  const newName = prompt("Insira o novo nome", name);
-  const newPrice = prompt("Insira o novo preço", price);
-  const newQuantity = prompt("Insira a nova quantidade", quantity);
-  if (newName === null || newPrice === null || newQuantity === null) {
-    alert("Valores não podem ser nulos");
-  } else if (
-    newName.length == 0 ||
-    newPrice.length == 0 ||
-    newQuantity.length == 0
-  ) {
+  const newName = prompt("Insira o novo nome", name).trim();
+  const newPrice = Number(prompt("Insira o novo preço", price));
+  const newQuantity = Number(prompt("Insira a nova quantidade", quantity));
+  if (!newName || !newPrice || !newQuantity) {
     alert("Valores não podem ser nulos");
   } else {
     await fetch(`http://localhost:3000/api/cars/${id}`, {
@@ -61,8 +66,8 @@ async function editCar(id, name, price, quantity) {
         quantity: newQuantity,
       }),
     });
-    fetchCars();
   }
+  fetchCars();
 }
 
 async function deleteCar(id) {
--- server.js
+++ server.js
@@ -22,14 +22,14 @@ db.connect((err) => {
   console.log("Connected to database.");
 });
 
-app.get("/api/cars", (req, res) => {
+app.get("/cars", (req, res) => {
   db.query("SELECT * FROM cars", (err, results) => {
     if (err) throw err;
     res.json(results);
   });
 });
 
-app.get("/api/cars/:id", (req, res) => {
+app.get("/cars/:id", (req, res) => {
   const { id } = req.params;
   db.query("SELECT * FROM cars WHERE id = ?", [id], (err, result) => {
     if (err) throw err;
@@ -37,26 +37,30 @@ app.get("/api/cars/:id", (req, res) => {
   });
 });
 
-app.post("/api/cars", (req, res) => {
+app.post("/cars", (req, res) => {
   const { name, price, quantity } = req.body;
   const car = { name, price, quantity };
-  db.query("INSERT INTO cars SET ?", car, (err, result) => {
-    if (err) throw err;
-    res.json({ id: result.insertId, ...car });
-  });
+  if (car) {
+    db.query("INSERT INTO cars SET ?", car, (err, result) => {
+      if (err) throw err;
+      res.json({ id: result.insertId, ...car });
+    });
+  }
 });
 
-app.put("/api/cars/:id", (req, res) => {
+app.put("/cars/:id", (req, res) => {
   const { id } = req.params;
   const { name, price, quantity } = req.body;
   const car = { name, price, quantity };
-  db.query("UPDATE cars SET ? WHERE id = ?", [car, id], (err, result) => {
-    if (err) throw err;
-    res.json({ id, ...car });
-  });
+  if (car) {
+    db.query("UPDATE cars SET ? WHERE id = ?", [car, id], (err, result) => {
+      if (err) throw err;
+      res.json({ id, ...car });
+    });
+  }
 });
 
-app.delete("/api/cars/:id", (req, res) => {
+app.delete("/cars/:id", (req, res) => {
   const { id } = req.params;
   db.query("DELETE FROM cars WHERE id = ?", [id], (err, result) => {
     if (err) throw err;
