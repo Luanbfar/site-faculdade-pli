5f110f3143016b25a1ed52ac24cab8030a80ad25
Luan Farias <luanbfarias@outlook.com>
2024-06-12 20:29:17 -0300

    Changed car-inventory style

--- client/src/modules/car-management.js
+++ client/src/modules/car-management.js
@@ -10,31 +10,63 @@ async function fetchCars() {
   const cars = await response.json();
   const carsDiv = document.getElementById("cars");
   carsDiv.innerHTML = "";
-  if (cars.length == 0) {
+
+  if (cars.length === 0) {
     const noCars = document.createElement("div");
     noCars.className =
       "container mt-5 d-flex justify-content-center align-items-center text-light";
     noCars.innerHTML = "Sem carros";
     carsDiv.appendChild(noCars);
+    return;
   }
+
+  const table = document.createElement("table");
+  table.className = "table table-striped table-hover text-light";
+
+  const thead = document.createElement("thead");
+  thead.className = "table-dark";
+  thead.innerHTML = `
+    <tr>
+      <th>Nome</th>
+      <th>Preço</th>
+      <th>Quantidade</th>
+      <th>Ações</th>
+    </tr>
+  `;
+  table.appendChild(thead);
+
+  const tbody = document.createElement("tbody");
+
   cars.forEach((car) => {
-    const carElement = document.createElement("div");
-    carElement.id = car.id;
-    carElement.className =
-      "container mt-5 p-1 d-flex justify-content-center align-items-center";
-    carElement.innerHTML = `
-          <p class="text-light me-3">${car.name} - R$${formatNumberBR(
-      car.price
-    )} - Quantidade: ${car.quantity}</p>
-          <button class="btn btn-secondary me-2" onclick="editCar(${car.id}, '${
+    const editBtnIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+  <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+</svg>`;
+
+    const deleteBtnIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>`;
+
+    const carRow = document.createElement("tr");
+    carRow.id = car.id;
+    carRow.innerHTML = `
+      <td>${car.name}</td>
+      <td>R$${formatNumberBR(car.price)}</td>
+      <td>${car.quantity}</td>
+      <td>
+        <button class="btn btn-secondary me-2" onclick="editCar(${car.id}, '${
       car.name
-    }', ${car.price}, ${car.quantity})">Editar</button>
-          <button class="btn btn-danger" onclick="deleteCar(${
-            car.id
-          })">Deletar</button>
-      `;
-    carsDiv.appendChild(carElement);
+    }', ${car.price}, ${car.quantity})">${editBtnIcon}</button>
+        <button class="btn btn-danger" onclick="deleteCar(${
+          car.id
+        })">${deleteBtnIcon}</button>
+      </td>
+    `;
+    tbody.appendChild(carRow);
   });
+
+  table.appendChild(tbody);
+  carsDiv.appendChild(table);
 }
 
 async function addCar(event) {
